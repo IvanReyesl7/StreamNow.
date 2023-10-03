da59ff16fcd347560f35cefe87283ef0af28a693
ivan.reyes <ivan_reyesl5@outlook.com>
2023-10-02 23:31:50 -0600

    Componentes

--- App.js
+++ App.js
@@ -1,20 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  FlatList,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+} from "react-native";
 
-export default function App() {
+import Producto from "./src/components/Producto";
+import Formulario from "./src/components/Formulario";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const App = () => {
+  const [productos, setProductos] = useState([]);
+  const [showForm, showSaveForm] = useState(false);
+
+  useEffect(() => {
+    const getProductosStorage = async () => {
+      try {
+        const productosStorage = await AsyncStorage.getItem("productos");
+        if (productosStorage) setProductos(JSON.parse(productosStorage));
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getProductosStorage();
+  }, []);
+
+  const eliminarProducto = (id) => {
+    const productosFiltrados = productos.filter(
+      (producto) => producto.id !== id
+    );
+    setCitas(productosFiltrados);
+    saveProductosStorage(JSON.stringify(productos));
+  };
+
+  const mostrarForm = () => showSaveForm(!showForm);
+
+  const cerrarTeclado = () => Keyboard.dismiss();
+
+  const saveProductosStorage = async (productosJSON) => {
+    try {
+      await AsyncStorage.setItem("productos", productosJSON);
+    } catch (error) {
+      console.log("error");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          cerrarTeclado();
+        }}
+      >
+        <SafeAreaView>
+          <View style={styles.contenedor}>
+            <Text style={styles.titulo}>Administrador de Productos</Text>
+            <View>
+              <TouchableHighlight
+                onPress={() => mostrarForm()}
+                style={styles.btnMostrarForm}
+              >
+                <View>
+                  <Text style={styles.textoMostrarForm}>
+                    {mostrarForm ? "Cancelar Crear producto" : "Crear Nuevo Producto"}
+                  </Text>
+                </View>
+              </TouchableHighlight>
+            </View>
+          </View>
+          <View style={styles.contenido}>
+            {mostrarForm ? (
+              <>
+                <Text style={styles.titulo}>Crear Nuevo Producto</Text>
+                <Formulario
+                  productos={productos}
+                  setProductos={setProductos}
+                  showSaveForm={showSaveForm}
+                  saveProductosStorage={saveProductosStorage}
+                />
+              </>
+            ) : (
+              <>
+                <Text style={styles.titulo}>
+                  {productos.length > 0
+                    ? "Administra tus productos"
+                    : "No hay productos, agrege uno"}
+                </Text>
+                <FlatList
+                  style={styles.listado}
+                  data={productos}
+                  renderItem={({ item }) => (
+                    <Producto item={item} eliminarProducto={eliminarProducto} />
+                  )}
+                  keyExtractor={(producto) => producto.id}
+                />
+              </>
+            )}
+          </View>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  contenedor: {
+    backgroundColor: "#C70039",
+  },
+  titulo: {
+    color: "#FFF",
+    marginTop: Platform.OS === "ios" ? 40 : 20,
+    marginBottom: 20,
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  contenido: {
+    marginHorizontal: "2.5%",
+  },
+  btnMostrarForm: {
+    padding: 10,
+    backgroundColor: "#C70039",
+    marginVertical: 10,
+  },
+  textoMostrarForm: {
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
+
+export default App;
---
+++ src/components/Producto.js
@@ -0,0 +1,61 @@
+import { View, Text, TouchableHighlight, StyleSheet } from "react-native";
+import React, { useState } from "react";
+
+const Producto = ({ item, eliminarProducto }) => {
+  const textoEliminar = (id) => {
+    console.log("eliminando..." + id);
+    eliminarProducto(id);
+  };
+
+  return (
+    <View style={styles.producto}>
+      <View>
+        <Text style={styles.label}>Pieza:</Text>
+        <Text style={styles.label}>{}</Text>
+      </View>
+
+      <View>
+        <Text style={styles.label}>Fecha de cambio:</Text>
+        <Text style={styles.label}>{}</Text>
+      </View>
+      <View>
+        <TouchableHighlight
+          onPress={() => textoEliminar(item.id)}
+          style={styles.btnEliminar}
+        >
+          <Text style={styles.textoEliminar}>Eliminar &times;</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  producto: {
+    backgroundColor: "FFF",
+    borderBottomColor: "E1E1E1",
+    borderStyle: "solid",
+    borderBottomWidth: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+
+  label: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginTop: 20,
+  },
+  texto: {
+    fontSize: 18,
+  },
+  btnEliminar: {
+    padding: 10,
+    backgroundColor: "red",
+    marginVertical: 10,
+  },
+  textoEliminar: {
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
